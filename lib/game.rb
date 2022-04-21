a61de9893df5999df804bbcc7435a958345c86fc
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2022-04-21 19:13:51 +0200

    Make our snake have nice moves

--- lib/game.rb
+++ lib/game.rb
@@ -8,15 +8,37 @@ class Game < Gosu::Window
     super Config::WINDOW_SIZE, Config::WINDOW_SIZE
     self.caption = "Le Wagon Snake"
 
+    @refresh_rate = 0.1
+    @last_timestamp = Time.now
+
     @snake = Snake.new
   end
 
   def update
-    # ...
+    return if (Time.now - @last_timestamp) < @refresh_rate
+
+    update_snake_direction
+    @snake.move
+
+    @last_timestamp = Time.now
   end
 
   def draw
     @snake.draw
+  end
+
+  private
+
+  def update_snake_direction
+    if Gosu.button_down?(Gosu::KB_RIGHT) && @snake.direction != "left"
+      @snake.direction = "right"
+    elsif Gosu.button_down?(Gosu::KB_DOWN) && @snake.direction != "up"
+      @snake.direction = "down"
+    elsif Gosu.button_down?(Gosu::KB_LEFT) && @snake.direction != "right"
+      @snake.direction = "left"
+    elsif Gosu.button_down?(Gosu::KB_UP) && @snake.direction != "down"
+      @snake.direction = "up"
+    end
   end
 end
 
--- lib/snake.rb
+++ lib/snake.rb
@@ -1,11 +1,24 @@
 require_relative "config"
 
 class Snake
+  attr_accessor :direction
+
   TILE_SIZE = Config::TILE_SIZE
 
   def initialize
     @x = TILE_SIZE
     @y = TILE_SIZE
+
+    @direction = "right"
+  end
+
+  def move
+    case @direction
+    when "right" then @x += TILE_SIZE
+    when "down" then @y += TILE_SIZE
+    when "left" then @x -= TILE_SIZE
+    when "up" then @y -= TILE_SIZE
+    end
   end
 
   def draw
